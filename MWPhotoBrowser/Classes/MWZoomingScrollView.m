91a445375fd00420c4a8539ac27190513e965afd
乐星宇 <lxy910428@qq.com>
2015-02-02 17:51:26 +0800

    add long press gesture support to image view

--- MWPhotoBrowser/Classes/MWZoomingScrollView.m
+++ MWPhotoBrowser/Classes/MWZoomingScrollView.m
@@ -411,4 +411,10 @@
     [self handleDoubleTap:CGPointMake(touchX, touchY)];
 }
 
+- (void)longPressDetectedOnImageView:(UIImageView *)imageView
+{
+    [_photoBrowser performSelector:@selector(savePhoto) withObject:nil afterDelay:0.2];
+}
+
+
 @end
